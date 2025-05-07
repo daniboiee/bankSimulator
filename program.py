ea8dd7d716e8bbd3ead6c0e5b29d69991f9ac1b7
Daniel Sarruf <daniel.sarruf@my.bdsc.school.nz>
2025-05-08 09:48:16 +1200

    started creating main()

--- program.py
+++ program.py
@@ -3,3 +3,17 @@
 # Function to make an account
 def makeAccount
     starting_balance = 10000
+
+def main():
+    account = None
+    choice = None
+    while choice != "5":
+        print("\n--- Bank Menu ---")
+        print("1. Create Account")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Show Transaction History")
+        print("5. Exit")
+        choice = input("Enter your choice (1-5): ")
+
+main()
