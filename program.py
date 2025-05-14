4e15d72346fac7541d9bc823b7c5fc9446165a2b
Daniel Sarruf <daniel.sarruf@my.bdsc.school.nz>
2025-05-15 09:47:01 +1200

    code pre-testing

--- program.py
+++ program.py
@@ -1,19 +1,54 @@
-# version1
+# Version 1 of the bank program
+
+balance = 0.0
+transaction_history = ""
 
 # Function to make an account
-def makeAccount
-    starting_balance = 10000
+def makeAccount():
+    balance += 10000.0
+    transaction_history = "Deposited 10000."
+
+def bankMenu():
+    choice = None
+    while choice != "4":
+        print("\n--- Bank Menu ---")
+        print("1. Deposit")
+        print("2. Withdraw")
+        print("3. Show Transaction History")
+        print("4. Exit")
+        choice = input("Enter your choice (1-4): ")
+        match choice:
+            case "1":
+                updateBalance(False)
+            case "2":
+                updateBalance(True)
+            case "3":
+                print(transaction_history)
+            case "4":
+                break
+            case _:
+    print("Thank you for using the program.")
+
+def updateBalance(isWithdraw):
+    print(f"Your current account balance is {balance}.")
+    money = input("Please insert how much money you want to move: ")
+    if type(money) not in (int, float) or (isWithdraw and balance < money): # Checks if the insert value is valid
+        print("What you input is invalid. Please try again.")
+        updateBalance(isWithdraw)
+
+    if isWithdraw:
+        balance -= money
+        transaction_history += f"\nWithdrew {int(money)}."
+    else:
+        balance += money
+        transaction_history += f"\nDeposited {int(money)}."
+
 
 def main():
-    account = None
-    choice = None
-    while choice != "5":
-        print("\n--- Bank Menu ---")
-        print("1. Create Account")
-        print("2. Deposit")
-        print("3. Withdraw")
-        print("4. Show Transaction History")
-        print("5. Exit")
-        choice = input("Enter your choice (1-5): ")
+    makeAccount()
+    bankMenu()
+    
 
 main()
+
+#& C:/Users/daniel.sarruf/AppData/Local/Programs/Python/Python312/python.exe C:\Users\daniel.sarruf\Desktop\school_code\assessment_bank\bankSimulator\program.py
